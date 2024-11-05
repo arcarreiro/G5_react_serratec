f7c17e95e0d8ab6b54336f985c2af1b161d5cd57
anafortuna <alobofortuna@gmail.com>
2024-11-05 12:56:57 -0300

    Tentando get produtos da API

--- olympia_suplementos/src/components/Produto.jsx
+++ olympia_suplementos/src/components/Produto.jsx
@@ -1,16 +1,19 @@
+import { Link } from 'react-router-dom/cjs/react-router-dom.min'
 import '../css/produto.css'
-const Produto = ({ id, url, nome, descricao, preco, categoria, estoque, onComprarClick, onAdicionarCarrinhoClick }) => {
+const Produto = ({ id, imgurl, nome, descricao, preco, categoria, estoque}) => {
     return (
         <>
             <div className="card" key={id}>
-                <img style={{width: 200}} src={url} />
+                <Link to='/item'><img style={{width: 200}} src={imgurl}/></Link>
                 <p>{nome}</p>
                 <p>{descricao}</p>
                 <p>{preco.toFixed(2)}</p>
-                <div className="botoes">
+                <p>{categoria}</p>
+                <p>{estoque}</p>
+                {/* <div className="botoes">
                     <button className='botao' onClick={() => { onAdicionarCarrinhoClick(id) }}>Adicionar ao carrinho</button>
                     <button className='botao' onClick={() => { onComprarClick(id) }}>Comprar</button> 
-                </div>
+                </div> */}
             </div>
         </>
     )
