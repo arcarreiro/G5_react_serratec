f52cfc604b8de923b0d3828eb82440e31f3df51e
arcarreiro <arcarreiro@gmail.com>
2024-11-05 13:03:40 -0300

    Context fix

--- olympia_suplementos/src/context/General.jsx
+++ olympia_suplementos/src/context/General.jsx
@@ -3,24 +3,27 @@ import { createContext, useState } from "react";
 const GeneralContext = createContext ({})
 
 const GeneralProvider = ({ children }) => {
+// pedido
 const [item, setItem] = useState('')
 const [quantidade, setQuantidade]= useState(0)
 const [valorTotal, setValorTotal]= useState(0)
+// produto
+const [produto, setProduto] = useState([])
 const [nomeProduto, setNomeProduto] = useState('')
 const [imgURL, setImgURL] = useState('')
 const [descricao, setDescricao] = useState('')
 const [preco, setPreco] = useState('')
 const [categoria, setCategoria] = useState('')
 const [estoque, setEstoque] = useState(0)
+// usuário
 const [username, setUsername] = useState('')
 const [senha, setSenha] = useState('')
 const [email, setEmail] = useState('')
 const [open, setOpen] = useState(false);
-
     return(
         <>
         <GeneralContext.Provider value={{ item, quantidade, valorTotal, setValorTotal, setItem, setQuantidade,
-            nomeProduto, imgURL, descricao, preco, categoria, estoque,
+            produto, setProduto, nomeProduto, imgURL, descricao, preco, categoria, estoque,
             setNomeProduto, setImgURL, setDescricao, setPreco, setCategoria, setEstoque,
             username, senha, email, setUsername, setSenha, setEmail, open, setOpen
         }}>
