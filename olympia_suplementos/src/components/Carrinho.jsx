3f1e076330fbc09c1f4fb9bfdcd60d583858dfb1
arcarreiro <arcarreiro@gmail.com>
2024-11-05 16:16:10 -0300

    Update foto home

--- olympia_suplementos/src/components/Carrinho.jsx
+++ olympia_suplementos/src/components/Carrinho.jsx
@@ -18,16 +18,16 @@ const Carrinho = () => {
         border: '2px solid #000',
         boxShadow: 24,
         p: 4,
-      };
+    };
 
-    const {open, setOpen} = useContext(GeneralContext)
+    const { open, setOpen } = useContext(GeneralContext)
     const handleOpen = () => setOpen(true);
     const handleClose = () => setOpen(false);
 
     return <>
-    <div className="cart" style={{cursor: 'pointer'}}>
-                            <PiShoppingCartBold onClick={handleOpen} size={30}/>
-                        </div>
+        <div className="cart" style={{ cursor: 'pointer' }}>
+            <PiShoppingCartBold onClick={handleOpen} size={30} />
+        </div>
         {/* <Button onClick={handleOpen}>Open modal</Button> */}
         <Modal
             open={open}
@@ -36,10 +36,12 @@ const Carrinho = () => {
             aria-describedby="parent-modal-description"
         >
             <Box sx={{ ...style, width: 400 }}>
-                <h2 id="parent-modal-title">Text in a modal</h2>
+                <h2 id="parent-modal-title" style={{textAlign: 'center'}}>Seu Carrinho</h2>
                 <p id="parent-modal-description">
                     Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
                 </p>
+                <h3>Valor Total: R$ 100,00</h3>
+                <button>Concluir pedido</button>
             </Box>
         </Modal>
     </>
