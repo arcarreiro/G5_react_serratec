d8979a30db14d50ce8447cd31d7b1a5068205709
anafortuna <alobofortuna@gmail.com>
2024-11-05 23:28:53 -0300

    Ajustes css itens e home

--- olympia_suplementos/src/components/Itens.jsx
+++ olympia_suplementos/src/components/Itens.jsx
@@ -1,18 +1,26 @@
-const Itens = ({ id, imgurl, nome, descricao, preco, onComprarClick}) => {
+import '../css/itens.css'
+const Itens = ({ id, imgurl, nome, descricao, preco, onComprarClick }) => {
     return (
         <>
-            <div className="card" key={id}>
-                <h2>{nome}</h2>
-                <img src={imgurl}/>
-                <p>{descricao}</p>
-                <h2>R$ {preco}</h2>
-                <div className="botoes">
-                    <button className='botao' onClick={() => { onComprarClick(id) }}>Comprar</button> 
+            <div className='cardItem' style={{
+                display: 'flex',
+                flexDirection: 'row',
+                padding: '10px',
+                gap: '50px',
+                justifyContent: 'center',
+                margin: '2rem'
+            }}>
+                <div className='imagem'>
+                    <img src={imgurl} />
                 </div>
-            </div>
-
-            <div imagem>
-
+                <div className='infos'>
+                    <h2>{nome}</h2>
+                    <p>{descricao}</p>
+                    <h2>R$ {preco}</h2>
+                    <div className="botoes">
+                        <button className='botao' onClick={() => { onComprarClick(id) }}>Comprar</button>
+                    </div>
+                </div>
             </div>
         </>
     )
